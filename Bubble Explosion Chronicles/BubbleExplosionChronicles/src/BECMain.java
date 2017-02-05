3fc312c1b5c7ed58f1a3fce591d2925661a561d6
ndzurche <ndzurche@mtu.edu>
2017-02-05 16:48:48 -0500

    Reorganized and added new functionality

--- Bubble Explosion Chronicles/BubbleExplosionChronicles/src/BECMain.java
+++ Bubble Explosion Chronicles/BubbleExplosionChronicles/src/BECMain.java
@@ -1,6 +1,9 @@
 import static arcadia.Game.HEIGHT;
 import static arcadia.Game.WIDTH;
+
 import arcadia.*;
+import arcadia.Button;
+import arcadia.Input;
 
 import java.awt.*;
 import java.io.File;
@@ -10,15 +13,21 @@ import javax.imageio.ImageIO;
 
 public class BECMain extends Game {
 
+    private Image bg;
     private Image nukeDukem;
+    private int nDX, nDY;
 
     public BECMain() {
        try {
-    	   nukeDukem = ImageIO.read(new File("src/random.png"));
+    	   bg = ImageIO.read(new File("src/images/backgrounds/random.png"));
+    	   nukeDukem = ImageIO.read(new File("src/images/NukeDukemSprite.png"));
        } catch (IOException e) {
     	   System.out.println("Hello");
        }
-        
+       
+       nDX = 240;
+       nDY = 20;
+       
         reset();    
     }
     
@@ -42,6 +51,10 @@ public class BECMain extends Game {
 
 	@Override
 	public void tick(Graphics2D graphics, Input input, Sound sound) {
-		graphics.drawImage(nukeDukem, 0, 0, WIDTH, HEIGHT, null);
+		graphics.drawImage(bg, 0, 0, WIDTH, HEIGHT, null);
+		graphics.drawImage(nukeDukem, nDX, nDY, 120, 160, null);
+		
+		if (nDX > 0) if (input.pressed(Button.L)) nDX -= 4;
+		if (nDX < 480) if (input.pressed(Button.R)) nDX += 4;
 	}
 }
